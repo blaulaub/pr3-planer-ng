94cc05263d3681d27e26404d67f816c3a8528311
Christian Fuchs <cfuchs77@gmail.com>
2018-02-19 15:29:14 +0100

    + added goods

---
+++ src/app/good.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { Good } from './good';
+import { GOODS } from './mock-goods';
+
+@Injectable()
+export class GoodService {
+
+  constructor() { }
+
+  getGoods(): Observable<Good[]> {
+    return of(GOODS);
+  }
+}
---
+++ src/app/mock-goods.ts
@@ -0,0 +1,24 @@
+import { Good } from './good';
+
+export const GOODS: Good[] = [
+  { name: "Wood" },
+  { name: "Bricks" },
+  { name: "Wheat" },
+  { name: "Fruits" },
+  { name: "Corn" },
+  { name: "Sugar" },
+  { name: "Hemp" },
+  { name: "Textiles" },
+  { name: "Metal" },
+  { name: "Cotton" },
+  { name: "Metal Goods" },
+  { name: "Dyes" },
+  { name: "Coffee" },
+  { name: "Cocoa" },
+  { name: "Tobacco" },
+  { name: "Meat" },
+  { name: "Clothing" },
+  { name: "Ropes" },
+  { name: "Rum" },
+  { name: "Bread" }
+];
