43819f58e7f64d24d615b7362cd401c6f4a98228
Christian Fuchs <cfuchs77@gmail.com>
2018-02-19 14:04:27 +0100

    + added default route (to games), removed top-nav list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { GameComponent } from './game/game.component';
 import { CitiesComponent } from './cities/cities.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/games', pathMatch: 'full' },
   { path: 'games', component: GamesComponent },
   { path: 'game/:gameId', component: GameComponent },
   { path: 'cities', component: CitiesComponent }
